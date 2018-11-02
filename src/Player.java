1070986196075d8599de70f70fff2f6abb20c759
Jacob Post <jacob.post.2020@student.lhschools.org>
2018-11-02 13:28:33 -0400

    Merging caused errors
    A lot of new errors appeared in the Player class when i pulled down the new code, i need to figure out how to resolve them and fix them.

--- src/Player.java
+++ src/Player.java
@@ -22,7 +22,7 @@ Scanner chose = new Scanner(System.in);
         for (int i = 0; i < dice.length; i++) {
             dice[i] = new Die();
 
-          
+
 
             //need to have each Player Object hold it's own score so that when all 13 rounds are over for each player,
             // a winner can be calculated and declared.
@@ -148,7 +148,7 @@ Arrays.fill(heldDie,false);
 //    answeredCorrectly = true;
 //}
             //need to figure out how to solve the two bugs that cause confusion within the user interface during a turn.
-    if(rollNumber<2){do {
+    if(rollNumber<2) do {
         System.out.print("Would you like to roll again? ");
         String reRollDice = reRoll.nextLine();
 
@@ -190,8 +190,7 @@ Arrays.fill(heldDie,false);
                  }
             }
 
-        System.out.println("placeholder text for when choosing what category to score roll under.");
-        int chose = 0;
+
 
 
             answeredCorrectly = true; //releases the user from the answeredCorrectly 'loop'
@@ -201,31 +200,7 @@ Arrays.fill(heldDie,false);
             answeredCorrectly = true; //releases the user from the answeredCorrectly 'loop'
 
 
-        while (chose < 1 || chose > 13) {
-            System.out.println("Please choose one of the following categories.");
-            CategoryUse();
 
-
-            // System.out.println("1=Aces\n2=Twos\n3=Threes\n4=Fours\n5=Fives\n6=Sixes\n7=3OfAKind\n8=4OfAKind\n9=SmallStraight\n10=LargeStraight\n11=FullHouse\n12=Yahtzee\n13=Chance");
-            chose = chose1.nextInt();
-
-
-
-            if (!rules.checkCategory(chose)) {
-                rules.checkCategoryInput(dice, chose);
-            } else {
-                System.out.println("You have previously selected that category and currently have a score of ");
-                chose = 0;
-            }
-            System.out.println();
-            System.out.println();
-            System.out.println();
-            for (int x = 1; x<=13;x++){
-                System.out.println("Category check for "+ x +" is " + (rules.checkCategory(x)));
-            }
-            System.out.println();
-            System.out.println();
-            System.out.println();
 
             //currently, the code gets stuck and prevents the player from progressing (or in case of FullHouse
             // or others that print text for testing values, keeps printing the text and would crash the
@@ -233,82 +208,115 @@ Arrays.fill(heldDie,false);
         }
     }
 
-    public void CategoryUse() {
-        for (int index = 1; index <= 13; index++) {
 
-            if (!rules.checkCategory(index)) {
-
-                String display = (index) + "= ";
-                switch (index) {
-                    case 1:
-                        display += "Aces";
-                        break;
-                    case 2:
-                        display += "Bces";
-                        break;
-                    case 3:
-                        display += "Cces";
-                        break;
-                    case 4:
-                        display += "Dces";
-                        break;
-                    case 5:
-                        display += "Eces";
-                        break;
-                    case 6:
-                        display += "Fces";
-                        break;
-                    case 7:
-                        display += "Gces";
-                        break;
-                    case 8:
-                        display += "Hces";
-                        break;
-                    case 9:
-                        display += "Ices";
-                        break;
-                    case 10:
-                        display += "Jces";
-                        break;
-                    case 11:
-                        display += "Kces";
-                        break;
-                    case 12:
-                        display += "Lces";
-                        break;
-                    case 13:
-                        display += "Mces";
-                        break;
-                }
-                System.out.println(display);
             }
         }
 
-    } while (!answeredCorrectly)/*||if(rollNumber<2)*/;}
+     while (!answeredCorrectly)/*||if(rollNumber<2)*/;
     else{rollNumber++;}
 }
-System.out.println("placeholder text for when choosing what category to score roll under.");
+
+//The Player class got royaly messed up during the merging, how.
+
+        System.out.println("placeholder text for when choosing what category to score roll under.");
+        int chose = 0;
+
+ while (chose < 1 || chose > 13){
+        System.out.println("Please choose one of the following categories.");
+        CategoryUse();
+
+
+        // System.out.println("1=Aces\n2=Twos\n3=Threes\n4=Fours\n5=Fives\n6=Sixes\n7=3OfAKind\n8=4OfAKind\n9=SmallStraight\n10=LargeStraight\n11=FullHouse\n12=Yahtzee\n13=Chance");
+        chose=chose1.nextInt();
+        }
+public void CategoryUse(){
+        for(int index=1;index<=13;index++){
+
+        if(!rules.checkCategory(index)){
+
+        String display=(index)+"= ";
+        switch(index){
+        case 1:
+        display+="Aces";
+        break;
+        case 2:
+        display+="Bces";
+        break;
+        case 3:
+        display+="Cces";
+        break;
+        case 4:
+        display+="Dces";
+        break;
+        case 5:
+        display+="Eces";
+        break;
+        case 6:
+        display+="Fces";
+        break;
+        case 7:
+        display+="Gces";
+        break;
+        case 8:
+        display+="Hces";
+        break;
+        case 9:
+        display+="Ices";
+        break;
+        case 10:
+        display+="Jces";
+        break;
+        case 11:
+        display+="Kces";
+        break;
+        case 12:
+        display+="Lces";
+        break;
+        case 13:
+        display+="Mces";
+        break;
+        }
+        System.out.println(display);
+
+        if(!rules.checkCategory(chose)){
+        rules.checkCategoryInput(dice,chose);
+        }else{
+        System.out.println("You have previously selected that category and currently have a score of ");
+        chose=0;
+        }
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        for(int x=1;x<=13;x++){
+        System.out.println("Category check for "+x+" is "+(rules.checkCategory(x)));
+        }
+        System.out.println();
+        System.out.println();
+        System.out.println();
+
+
+        System.out.println("placeholder text for when choosing what category to score roll under.");
         System.out.println("1=Aces\n2=Twos\n3=Threes\n4=Fours\n5=Fives\n6=Sixes.");
         //below line is supposed to call the instance of the Rules class that the current player has, and then
         //calculate the score of the roll before starting the next player's turn.
 //rules.checkCategory();
-           // System.out.println("i at end of loop:"+rollNumber);
+        // System.out.println("i at end of loop:"+rollNumber);
         System.out.println();
         }
 //need to call up the Rules class to calculate the score for the round
-       // return getScore();
+// return getScore();
 
-    private static void printDice2(Die[] dice){
+private static void printDice2(Die[]dice){
         for(int i=0;i<5;i++){System.out.print(dice[i].getFaceValue()+" ");}
-    }
+        }
 
-    private static void printDice(Die[] dice){ // Prints graphical representation of dice in a row
+private static void printDice(Die[]dice){ // Prints graphical representation of dice in a row
 
-        String[] printRows = new String[5]; // We need rows because we want the dice to be all in a row.
+        String[]printRows=new String[5]; // We need rows because we want the dice to be all in a row.
         Arrays.fill(printRows,""); // Init. array
-        for(int i=0;i<5;i++) {
-            int faceVal = dice[i].getFaceValue(); // Stored so we don't need to keep calling the method
-            if(faceVal<1||faceVal>6){printRows[2]+="[!]";} // No graphical representation of the faceValue found
+        for(int i=0;i<5;i++){
+        int faceVal=dice[i].getFaceValue(); // Stored so we don't need to keep calling the method
+        if(faceVal<1||faceVal>6){printRows[2]+="[!]";} // No graphical representation of the faceValue found
 
             /* ___________________________________________________________________________________________________________
               |In order to compress the code, I've thought of a way to make all of the dice in the same five lines.       |
@@ -317,71 +325,80 @@ System.out.println("placeholder text for when choosing what category to score ro
               |Some dots on the die are shared between different faces, creating different conditions for them to appear. |
                ‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾ */
 
-            // - Text Dice Creation -
-            //printRows[0]+="---"+faceVal+"--- "; // DEBUG (it works!)
-            //printRows[0]+=" ______  ";
-            printRows[1]+=boolDot(faceVal!=1)+"   "+boolDot(faceVal>3)+"     ";
-            printRows[2]+=boolDot(faceVal==6)+" "+boolDot(faceVal%2==1)+" "+boolDot(faceVal==6)+"     ";
-            printRows[3]+=boolDot(faceVal>3)+"   "+boolDot(faceVal>1)+"     ";
-            //printRows[4]+=" ‾‾‾‾‾‾‾‾  ";
+        // - Text Dice Creation -
+        //printRows[0]+="---"+faceVal+"--- "; // DEBUG (it works!)
+        //printRows[0]+=" ______  ";
+        printRows[1]+=boolDot(faceVal!=1)+"   "+boolDot(faceVal>3)+"     ";
+        printRows[2]+=boolDot(faceVal==6)+" "+boolDot(faceVal%2==1)+" "+boolDot(faceVal==6)+"     ";
+        printRows[3]+=boolDot(faceVal>3)+"   "+boolDot(faceVal>1)+"     ";
+        //printRows[4]+=" ‾‾‾‾‾‾‾‾  ";
         }
         for(int i=0;i<5;i++){
-            System.out.println(printRows[i]);
+        System.out.println(printRows[i]);
         }
-    }
+        }
 
-    private static String boolDot(boolean condition){ // If the condition is true, return a "dot" to be put on the die.
-       if(condition){return "●";} else{return " ";}
-    }
+private static String boolDot(boolean condition){ // If the condition is true, return a "dot" to be put on the die.
+        if(condition){return"●";}else{return" ";}
+        }
 
-    // promptInt also checks for invalid inputs
-    private static int promptInt(String prompt) throws InputMismatchException {
-        Scanner sc = new Scanner(System.in);
+// promptInt also checks for invalid inputs
+private static int promptInt(String prompt)throws InputMismatchException{
+        Scanner sc=new Scanner(System.in);
         int output;
         try{
-            output = sc.nextInt();
+        output=sc.nextInt();
         }
-        catch (InputMismatchException e){
-            output = -1; // Set the output to -1 if an error is found
+        catch(InputMismatchException e){
+        output=-1; // Set the output to -1 if an error is found
         }
 
         return output;
-    }
+        }
 
-    private static int[] promptIntList(String prompt) throws InputMismatchException{
+private static int[]promptIntList(String prompt)throws InputMismatchException{
         System.out.print(prompt+" ");
-        Scanner sc = new Scanner(System.in);
-        String input = sc.nextLine();
-        int items = 1;
+        Scanner sc=new Scanner(System.in);
+        String input=sc.nextLine();
+        int items=1;
         for(int i=0;i<input.length();i++){if(input.substring(i,i+1).equals(",")){items++;}} // Count up the number of items in the list
-        int[] output = new int[items];
+        int[]output=new int[items];
         for(int i=0;i<items;i++){
-            try{output[i]=Integer.parseInt(fetchParam(input,i));} // Error checking
-            catch(Exception e){Arrays.fill(output,-1);return output;}
+        try{output[i]=Integer.parseInt(fetchParam(input,i));} // Error checking
+        catch(Exception e){Arrays.fill(output,-1);return output;}
         }
         return output;
-    }
+        }
 
-    private static String fetchParam(String str, int index){
-        int currentIndex = 0;
-        int lastDelimPos = -1;
+private static String fetchParam(String str,int index){
+        int currentIndex=0;
+        int lastDelimPos=-1;
         int i;
         for(i=0;i<str.length();i++){
-            if(str.substring(i,i+1).equals(",")){
-                if(currentIndex==index){return str.substring(lastDelimPos+1,i);}
-                currentIndex++;
-                lastDelimPos=i;
-            }
+        if(str.substring(i,i+1).equals(",")){
+        if(currentIndex==index){return str.substring(lastDelimPos+1,i);}
+        currentIndex++;
+        lastDelimPos=i;
+        }
         }
         return str.substring(lastDelimPos+1,i);
-    }
+        }
 
-    private static boolean inIntArr(int query,int[] arr){
+private static boolean inIntArr(int query,int[]arr){
         for(int i=0;i<arr.length;i++){
-            if(arr[i]==query){return true;}
+        if(arr[i]==query){return true;}
         }
         return false;
 
-    }
+        }
 
-}
+        }
+
+        }
+
+
+
+
+
+
+
